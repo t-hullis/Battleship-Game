a3c7cfe0e7a0ca4ce14c6e111ea2191a086373a4
t-hullis <t.hullis@yahoo.com>
2022-08-03 13:47:45 +0000

    start creating class for board object taking in the size, number of ships and board

--- run.py
+++ run.py
@@ -4,3 +4,13 @@ board_size = int(input("Please enter the size of the baord : " ))
 print(board_size)
 num_of_ships = int(input("Please enter the amount of ships on the board : "))
 print(num_of_ships)
+
+class GameBoard:
+    '''
+    Class for the game board
+    '''
+    def __init__(self, board_size, num_of_ships, board):
+        self.board_size = board_size
+        self.num_of_ships = num_of_ships
+        self.board = board
+
