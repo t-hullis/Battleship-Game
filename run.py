50bfc314fc11abfc737cd3eccedd40d1e5af1516
t-hullis <t.hullis@yahoo.com>
2022-08-03 20:56:20 +0000

    style the board

--- run.py
+++ run.py
@@ -1,25 +1,34 @@
-import random
-
-board_size = int(input("Please enter the size of the baord : "))
-print(board_size)
-num_of_ships = int(input("Please enter the amount of ships on the board : "))
-print(num_of_ships)
+import random 
 
 
 class GameBoard:
     '''
     Class for the game board
     '''
-    def __init__(self, board_size, num_of_ships):
+    def __init__(self, board_size, num_of_ship):
         self.board_size = board_size
-        self.num_of_ships = num_of_ships
+        self.num_of_ship = num_of_ship
         self.board = [["~" for x in range(board_size)] for y in range(board_size)]
 
-    def print_board(self):
-        print([" ".join(i) for i in range(board_size)])
+    def print_board(self, board_size):
+        '''
+        This function sets up the board for the player 
+        '''
+        for i in range(board_size):
+            print(f"{i + 1} ", end=" ")
         for row in self.board:
-            print("  ".join(row))
+            label = board_size
+            print(label, "  ".join(row))
+            label = label - 1
 
 
-game_board = GameBoard(board_size, num_of_ships)
-game_board.print_board()
+board_size = int(input("Please enter the size of the baord : "))
+print(board_size)
+num_of_ship = int(input("Please enter the amount of ships on the board : "))
+print(num_of_ship)
+
+
+game_board = GameBoard(board_size, num_of_ship)
+
+game_board.print_board(board_size)
+
